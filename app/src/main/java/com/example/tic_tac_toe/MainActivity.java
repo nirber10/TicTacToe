9004389f9eec1371eedf64bb1d33e5569f5a2367
NirBerman-app <dhdhggffv@gmail.com>
2023-12-07 15:30:38 +0200

    First java

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.tic_tac_toe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String turn;
+    String[][] board;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
+        onNewGame();
+    }
+
+    private void onNewGame() {
+        board = new String[3][3];
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                board[row][col] = new String();
+
+        turn = "X";
+        count = 0;
     }
 }
